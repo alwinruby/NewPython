e6e9798d742d64ce096988be2029444e51f7f402
asolanky <alwinsolanky@gmail.com>
2019-10-27 15:09:41 +0000

    if-demo.py

---
+++ if-demo.py
@@ -0,0 +1,18 @@
+# if boolean_expression:
+#     some commands
+
+# check a number is even or odd
+# num % 2
+
+# num = 4
+
+num = 5
+
+remainder = num % 2
+
+if remainder == 0:
+    print(num,' is even')
+else:
+    print(num, ' is odd')
+
+
